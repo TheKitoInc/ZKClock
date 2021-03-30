35d4af440223ee86ac361948e3aa641541155da7
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-30 09:06:08 -0300

    Revert "timezone change to arg"
    This reverts commit 9b019c7781753e9f0127d728ae2449305e64d96f.

--- ZKClockSetTime.py
+++ ZKClockSetTime.py
@@ -8,17 +8,15 @@ from datetime import datetime
 import sys
 import time
 
-host = sys.argv[len(sys.argv)-2]
-timeZoneOffSet = sys.argv[len(sys.argv)-1]
+host = sys.argv[len(sys.argv)-1]
 
 conn = None
 
 zk = ZK(host, port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False)
 try:
-        newtime = datetime.today() + timeZoneOffSet
-
         conn = zk.connect()
 
+        newtime = datetime.today()
         conn.set_time(newtime)
 
         conn.enable_device()
